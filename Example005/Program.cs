c8240692050596e2c59028bcb4d4fe1a2eb3ee57
Katrin85 <Em-1985@yandex.ru>
2022-08-05 13:19:58 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(1, 10); // случайное число от 1 до 9
+Console.WriteLine("Певое число " + numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("Второе число " + numberB);
+int result = numberA * numberB;
+Console.WriteLine("Произведение " + result);
+
+

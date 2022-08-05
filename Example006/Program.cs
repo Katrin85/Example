7e5ad7b4c17c64bce7d1b00821ad387330c31340
Katrin85 <Em-1985@yandex.ru>
2022-08-05 16:22:22 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите своё имя");
+string username = Console.ReadLine ();
+if (username.ToLower() == "катя")
+{
+   Console.WriteLine ("Ура, наша Катя заглянула!");
+}
+else if (username.ToLower() == "леня")
+{
+    Console.WriteLine ("Ооооо, Лёнчик давно тебя не было видно!");
+}
+else
+{ 
+   Console.WriteLine ("Привет, " + username);
+   Console.WriteLine ("Сколько тебе лет?");
+   string years = Console.ReadLine ();
+   Console.WriteLine (username + ", ты уже большой(ая). Тебе  " + years + " лет"); 
+   Console.WriteLine ("Будем знакомы!");
+}
